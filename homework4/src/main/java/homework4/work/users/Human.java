a06104ab0b3a637349cd2cd6d9773b92f4e86e05
Юрий Мусихин <vensop@gmail.com>
2024-02-07 00:44:08 +0300

    homework4

--- homework4/src/main/java/homework4/work/users/Human.java
+++ homework4/src/main/java/homework4/work/users/Human.java
@@ -4,11 +4,19 @@ public class Human {
     String name;
     String pass;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
     @Override
     public String toString() {
         return
-                " name='" + name + '\'' +
-                ", pass='" + pass + " ";
+                name +
+                ",your password=' " + pass + " ";
     }
 }
 
