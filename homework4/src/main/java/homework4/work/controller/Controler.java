f2487d71c033d67fe18bf66fb37854362728d385
Юрий Мусихин <vensop@gmail.com>
2024-02-06 21:27:55 +0300

    update

--- homework4/src/main/java/homework4/work/controller/Controler.java
+++ homework4/src/main/java/homework4/work/controller/Controler.java
@@ -3,11 +3,12 @@ package homework4.work.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Controler {
 
-    @GetMapping("/hello")
+    @RequestMapping("/hello")
     public String getHello(Model model){
         model.addAttribute("hello","heloe");
         return "hello";
